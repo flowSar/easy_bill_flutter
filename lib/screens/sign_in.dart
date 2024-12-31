1cad3b8abd3cd92dcfc9fe5f1486e115793ffa2c
flowsar <brahim.sar151@gmail.com>
2024-12-31 17:56:58 +0100

    add firebase to the project

---
+++ lib/screens/sign_in.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text('sign_in'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/sign_up.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text('sign_up'),
+        ),
+      ),
+    );
+  }
+}
